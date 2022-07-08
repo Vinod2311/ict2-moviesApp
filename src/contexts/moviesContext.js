ba93e81017353d72d56b5bb8141452f4fcbd7ae2
Vinod-Yadav <bkvinodyadav@hotmail.com>
2022-07-08 19:36:43 +0100

    Add review and fix storybook stories.

--- src/contexts/moviesContext.js
+++ src/contexts/moviesContext.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 export const MoviesContext = React.createContext(null);
 
 const MoviesContextProvider = (props) => {
+  const [myReviews, setMyReviews] = useState({});
   const [favourites, setFavourites] = useState([]);
 
   const addToFavourites = (movie) => {
@@ -17,17 +18,21 @@ const MoviesContextProvider = (props) => {
     setFavourites(favourites.filter((mId) => mId !== movie.id));
   };
 
+  const addReview = (movie, review) => {
+    setMyReviews({ ...myReviews, [movie.id]: review });
+  };
+
   return (
     <MoviesContext.Provider
       value={{
         favourites,
         addToFavourites,
         removeFromFavourites,
+        addReview,
       }}
     >
       {props.children}
     </MoviesContext.Provider>
   );
 };
-
 export default MoviesContextProvider;
