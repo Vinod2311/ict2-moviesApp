53bc63d6c4d1854b39564ae30ef3ae753d3bae29
Vinod-Yadav <bkvinodyadav@hotmail.com>
2022-07-09 16:22:56 +0100

    New button for upcoming movies page.

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -3,7 +3,7 @@ import PageTemplate from "../components/templateMovieListPage";
 import { useQuery } from 'react-query'
 import Spinner from '../components/spinner'
 import {getUpcomingMovies} from '../api/tmdb-api'
-import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
+import PlaylistAddIcon from "@material-ui/icons/PlaylistAdd";
 
 const UpcomingMoviesPage = (props) => {
   const {  data, error, isLoading, isError }  = useQuery('discover upcoming movies', getUpcomingMovies)
@@ -23,7 +23,7 @@ const UpcomingMoviesPage = (props) => {
       title='Upcoming Movies'
       movies={movies}
       action={(movie) => {
-        return <AddToFavouritesIcon movie={movie} />
+        return <PlaylistAddIcon movie={movie} />
       }}
       />  
   );
